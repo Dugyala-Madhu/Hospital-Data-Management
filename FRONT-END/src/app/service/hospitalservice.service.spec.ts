84b4c9e131a63ab30e0efb282e3f62b083185e5a
Dugyala-Madhu <112916889+Dugyala-Madhu@users.noreply.github.com>
2022-09-19 14:52:37 +0530

    Add files via upload

---
+++ FRONT-END/src/app/service/hospitalservice.service.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HospitalserviceService } from './hospitalservice.service';
+
+describe('HospitalserviceService', () => {
+  let service: HospitalserviceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(HospitalserviceService);
+  });
+
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+});
+  
+});
